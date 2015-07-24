a0da410aa4fceaae5afca164f52ab6654f0e85b6
Jennifer Jones <jybjones@gmail.com>
2015-07-24 15:09:58 -0500

    separated logic, but not 100% functioning

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 #!/usr/bin/env node
+var getMonth = require('./lib/month');
+var getYear = require('./lib/year');
+
+var month = process.argv[2];
+var year = process.argv[3];
+var Calendar = require('./lib/zellers');
+
+Calendar.zellers(month, year);
