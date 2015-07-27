dfd0c709f48c9b76510f3f6f77210dcd2a0f1ec1
Jennifer Jones <jybjones@gmail.com>
2015-07-26 19:25:12 -0500

    fixed zellers function added eslint

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ var getYear = require('./lib/year');
 
 var month = process.argv[2];
 var year = process.argv[3];
-var Calendar = require('./lib/zellers');
+var Zellers = require('./lib/zellers');
 
-Calendar.zellers(month, year);
+Zellers(month, year);
--- test/unit/zellers.test.js
+++ test/unit/zellers.test.js
@@ -1,12 +1,11 @@
 #!/usr/bin/env node
 
 var should = require('chai').should();
-var zellers = require(process.cwd() + '/lib/zellers.js');
 var month = require(process.cwd() + '/lib/month.js');
-var cp = require('child_process');
 
 
 describe('zellers', function (){
+ var zellers = require(process.cwd() + '/lib/zellers.js');
  it('should return correct weekday for July 2015', function(){
    zellers(3, 2015).should.equal(1);
  });
